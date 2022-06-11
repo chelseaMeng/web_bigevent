b9d98dd586d0af65efa5eca6ceb84e1b69ec2e73
chelseaM <290815385@qq.com>
2022-06-11 17:15:25 +0800

    完成了user的功能

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,49 @@
+$(function () {
+  const form = layui.form;
+  //自定义校验规则
+  form.verify({
+    nickname: (value) => {
+      if (value.length > 6) return '昵称长度不能超过6个字符';
+    },
+  });
+
+  //获取用户基本信息
+  const initUserInfo = () => {
+    $.ajax({
+      method: 'GET',
+      url: '/my/userinfo',
+      success: (res) => {
+        if (res.status !== 0) return layer.msg('获取用户信息失败!');
+        layer.msg('获取用户信息成功!');
+        console.log(res);
+        //填充表单
+        form.val('formUserInfo', res.data);
+      },
+    });
+  };
+
+  initUserInfo();
+
+  //重置表单
+  $('#btnReset').click((e) => {
+    e.preventDefault();
+    initUserInfo();
+  });
+
+  //更新用户信息
+  $('.layui-form').submit(function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'post',
+      url: '/my/userinfo',
+      data: $(this).serialize(),
+      success: (res) => {
+        console.log(res);
+        if (res.status !== 0) return layer.msg('更新用户信息失败!');
+        layer.msg('更新用户信息成功!');
+        //通知父页面 更新用户信息
+        window.parent.getUserInfo();
+      },
+    });
+  });
+});
